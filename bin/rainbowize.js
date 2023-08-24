98f69d784c7c6d66a0e48276f94d062fbfecc00a
Semih Arslan <arslan_semih@yahoo.com>
2023-08-24 19:01:34 +0300

    lines are colorized

---
+++ bin/rainbowize.js
@@ -0,0 +1,48 @@
+const readline = require('readline');
+
+/**
+ * Calculate RGB values based on an index.
+ * @param {number} i - The index used for calculations.
+ * @returns {number[]} An array of RGB values.
+ */
+function rgb(i) {
+    const f = 0.1;
+
+    return [
+        Math.floor(Math.sin(f * i + 0) * 127 + 128),
+        Math.floor(Math.sin(f * i + (2 * Math.PI) / 3) * 127 + 128),
+        Math.floor(Math.sin(f * i + (4 * Math.PI) / 3) * 127 + 128)
+    ];
+}
+
+/**
+ * Rainbowize the input from stdin and print it to stdout.
+ */
+async function main() {
+    const stdin = process.stdin;
+
+    // Check if input is coming from a TTY (terminal) or not (piped).
+    if (stdin.isTTY) {
+        console.log("The command is intended to work with pipes.");
+        console.log("Usage: fortune | rainbowize");
+        return;
+    }
+
+    // Create a readline interface to read from stdin.
+    const reader = readline.createInterface({
+        input: stdin
+    });
+
+    // Read each line from stdin and print it to stdout with a different color.
+    let j = 0;
+    for await (const input of reader) {
+        // Calculate RGB values based on the line number.
+        const [r, g, b] = rgb(j);
+
+        // Print the line to stdout with the calculated color.
+        console.log(`\x1b[38;2;${r};${g};${b}m${input}\x1b[0m`);
+        j++;
+    }
+}
+
+main();
