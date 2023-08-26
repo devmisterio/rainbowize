ee1a2373590a958ab2eaeb37fad8994d33bc5bf1
devmisterio <arslan_semih@yahoo.com>
2023-08-26 17:24:43 +0300

    added rome and formatted files

--- bin/rainbowize.js
+++ bin/rainbowize.js
@@ -2,63 +2,63 @@
 
 const readline = require("readline");
 const yargs = require("yargs/yargs");
-const {hideBin} = require("yargs/helpers");
-const {rainbowizeInput} = require("../lib/utils/input");
-const {ConfigManager} = require("../lib/class/ConfigManager");
+const { hideBin } = require("yargs/helpers");
+const { rainbowizeInput } = require("../lib/utils/input");
+const { ConfigManager } = require("../lib/class/ConfigManager");
 
 async function main() {
-  const stdin = process.stdin;
+	const stdin = process.stdin;
 
-  const argv = yargs(hideBin(process.argv))
-    .option("help", {
-      alias: "h",
-      describe: "Show usage information",
-    })
-    .option("scheme", {
-      alias: "s",
-      describe: "Color scheme for rainbowizing",
-    })
-    .option("bold", {
-      alias: "b",
-      describe: "Apply bold text effect",
-      boolean: true,
-    })
-    .option("italic", {
-      alias: "i",
-      describe: "Apply italic text effect",
-      boolean: true,
-    }).argv;
+	const argv = yargs(hideBin(process.argv))
+		.option("help", {
+			alias: "h",
+			describe: "Show usage information",
+		})
+		.option("scheme", {
+			alias: "s",
+			describe: "Color scheme for rainbowizing",
+		})
+		.option("bold", {
+			alias: "b",
+			describe: "Apply bold text effect",
+			boolean: true,
+		})
+		.option("italic", {
+			alias: "i",
+			describe: "Apply italic text effect",
+			boolean: true,
+		}).argv;
 
-  const configManager = new ConfigManager();
-  const config = configManager.loadConfig();
+	const configManager = new ConfigManager();
+	const config = configManager.loadConfig();
 
-  const defaultScheme = config.customScheme || false;
-  const defaultBold = config.defaultBold || false;
-  const defaultItalic = config.defaultItalic || false;
+	const defaultScheme = config.customScheme || false;
+	const defaultBold = config.defaultBold || false;
+	const defaultItalic = config.defaultItalic || false;
 
-  if (argv.help) {
-    console.log("Usage: <cmd> | rainbowize");
-    console.log("--help: Show usage information");
-    return;
-  }
+	if (argv.help) {
+		console.log("Usage: <cmd> | rainbowize");
+		console.log("--help: Show usage information");
+		return;
+	}
 
-  if (stdin.isTTY) {
-    console.log("The command is intended to work with pipes.");
-    console.log("Usage: <cmd> | rainbowize");
-    return;
-  }
+	if (stdin.isTTY) {
+		console.log("The command is intended to work with pipes.");
+		console.log("Usage: <cmd> | rainbowize");
+		return;
+	}
 
-  const reader = readline.createInterface({
-    input: stdin,
-  });
+	const reader = readline.createInterface({
+		input: stdin,
+	});
 
-  const scheme = argv.scheme || defaultScheme;
-  const bold = argv.bold || defaultBold;
-  const italic = argv.italic || defaultItalic;
+	const scheme = argv.scheme || defaultScheme;
+	const bold = argv.bold || defaultBold;
+	const italic = argv.italic || defaultItalic;
 
-  await rainbowizeInput(reader, scheme, bold, italic);
+	await rainbowizeInput(reader, scheme, bold, italic);
 }
 
 main().catch((error) => {
-  console.error("An unhandled error occurred:", error);
+	console.error("An unhandled error occurred:", error);
 });
