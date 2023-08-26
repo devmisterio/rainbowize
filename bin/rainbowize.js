8d7b59637c2cab38735657a24746fb906e4003e4
devmisterio <arslan_semih@yahoo.com>
2023-08-26 17:40:49 +0300

    Added CommandProcessor class

--- bin/rainbowize.js
+++ bin/rainbowize.js
@@ -1,56 +1,23 @@
 #!/usr/bin/env node
 
-const readline = require("readline");
-const yargs = require("yargs/yargs");
-const { hideBin } = require("yargs/helpers");
 const { rainbowizeInput } = require("../lib/utils/input");
 const { ConfigManager } = require("../lib/class/ConfigManager");
+const { CommandProcessor } = require("../lib/class/CommandProcessor");
 
 async function main() {
-	const stdin = process.stdin;
-
-	const argv = yargs(hideBin(process.argv))
-		.option("help", {
-			alias: "h",
-			describe: "Show usage information",
-		})
-		.option("scheme", {
-			alias: "s",
-			describe: "Color scheme for rainbowizing",
-		})
-		.option("bold", {
-			alias: "b",
-			describe: "Apply bold text effect",
-			boolean: true,
-		})
-		.option("italic", {
-			alias: "i",
-			describe: "Apply italic text effect",
-			boolean: true,
-		}).argv;
-
+	const commandProcessor = new CommandProcessor();
 	const configManager = new ConfigManager();
+
+	commandProcessor.handleTTYCheck();
+	const argv = commandProcessor.parseArguments();
+
 	const config = configManager.loadConfig();
 
 	const defaultScheme = config.customScheme || false;
 	const defaultBold = config.defaultBold || false;
 	const defaultItalic = config.defaultItalic || false;
 
-	if (argv.help) {
-		console.log("Usage: <cmd> | rainbowize");
-		console.log("--help: Show usage information");
-		return;
-	}
-
-	if (stdin.isTTY) {
-		console.log("The command is intended to work with pipes.");
-		console.log("Usage: <cmd> | rainbowize");
-		return;
-	}
-
-	const reader = readline.createInterface({
-		input: stdin,
-	});
+	const reader = commandProcessor.createInputReader();
 
 	const scheme = argv.scheme || defaultScheme;
 	const bold = argv.bold || defaultBold;
